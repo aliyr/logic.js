62b2f13a2a81c2cb519e897a8e54caaf0427b399
MSeif <m.seif@jabama.com>
2022-12-31 11:28:40 +0330

    feat(webpack): add fallback config in dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,51 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    entry: './src/index.js',
-    devServer: {
-        static: './index.html',
-        hot: true,
-        client: {
-            overlay: {
-                errors: true,
-                warnings: false,
-            },
+  mode: "development",
+  entry: "./src/index.js",
+  devServer: {
+    historyApiFallback: true,
+    static: "./index.html",
+    hot: true,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: false,
+      },
+    },
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Hot Module Replacement",
+      template: "./index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+            plugins: [
+              "./babel-plugins/custom-jsx-plugin",
+              "@babel/plugin-syntax-jsx",
+            ],
+          },
         },
-    },
-    devtool: 'inline-source-map',
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Hot Module Replacement',
-            template: './index.html'
-        }),
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            ['@babel/preset-env', { targets: "defaults" }]
-                        ],
-                        plugins: ['./babel-plugins/custom-jsx-plugin', '@babel/plugin-syntax-jsx']
-                    }
-                }
-            },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            }
-        ]
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 };
