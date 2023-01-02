b3b3dabc21f98a6549d0526a0548a353cb5255b6
Alious <a.usefi@jabama.com>
2023-01-02 15:50:59 +0330

    feat: router route modification

--- src/routing/router.js
+++ src/routing/router.js
@@ -4,6 +4,8 @@ export class Router {
         this.routes = routes
         this.activeRoute = null
 
+        this.manipulateRoutes()
+
         this.createAddLocationEvent()
 
         this.loadRoute({path: window.location.pathname})
@@ -30,6 +32,41 @@ export class Router {
             window.dispatchEvent(new Event('locationchange'));
             return ret;
         };
+    }
+
+    manipulateRoutes() {
+        this.routes = this.addFullPath(this.routes, '')
+        this.routes = this.flattenRoutes(this.routes)
+    }
+
+    addFullPath(routesArr = [], parentRoute = '') {
+        return routesArr.map((item) => {
+            return {
+                ...item,
+                fullRoute: parentRoute + item.path,
+                children: this.addFullPath(item.children, parentRoute + item.path)
+            }
+        })
+    }
+
+    flattenRoutes(routes) {
+        const modifiedRoutes = []
+
+        const pushRoutesToRoot = (routeArr) => {
+            routeArr.forEach(r => {
+                if (!r.children || !r.children.length) {
+                    modifiedRoutes.push(r)
+                } else {
+                    pushRoutesToRoot(r.children)
+                    delete r.children
+                    modifiedRoutes.push(r)
+                }
+            })
+        }
+
+        pushRoutesToRoot(routes)
+
+        return modifiedRoutes
     }
 
 }
--- src/usage/component/product/index.js
+++ src/usage/component/product/index.js
@@ -1,4 +1,5 @@
 import Component from "@logic/component";
+import {RouterLink} from "@logic/routing/router-link";
 
 export class Product extends Component {
     constructor(props) {
@@ -9,6 +10,27 @@ export class Product extends Component {
         return (
             <div>
                 product main page
+                <RouterLink props={
+                    {
+                        name: 'productDetail',
+                        params: {
+                            id: '1'
+                        }
+                    }
+                }>
+                        <span>
+                            detail 1
+                        </span>
+                </RouterLink>
+                <RouterLink props={
+                    {
+                        name: 'productList'
+                    }
+                }>
+                        <span>
+                            list
+                        </span>
+                </RouterLink>
                 {/*<RouterOutlet />*/}
             </div>
         )
